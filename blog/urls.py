6a5944637715a131f09250fad8e96a1400f0db87
jackevans47 <jack_evans47@outlook.com>
2024-05-29 18:29:59 +0100

    Add functionallity to blog article with links

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
-from .views import Home
+from .views import Home, Article
 
-urlpatterns = [path("", Home.as_view(), name="home")]
+urlpatterns = [
+    path("", Home.as_view(), name="home"),
+    path("article/<int:pk>", Article.as_view(), name="article-details"),
+]
--- blog/views.py
+++ blog/views.py
@@ -6,3 +6,8 @@ from blog.models import Post
 class Home(ListView):
     model = Post
     template_name = "home.html"
+
+
+class Article(DetailView):
+    model = Post
+    template_name = "article.html"
