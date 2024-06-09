77d10c2b5177884589ef2cd444af9ec38c0f44b4
jackevans47 <jack_evans47@outlook.com>
2024-06-09 09:12:29 +0100

    Add profile card to posts

--- blog/models.py
+++ blog/models.py
@@ -17,6 +17,14 @@ class Category(models.Model):
         return reverse("home")
 
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    bio = models.TextField()
+
+    def __str__(self):
+        return str(self.user)
+
+
 # Create blog posts
 class Post(models.Model):
     title = models.CharField(max_length=255)
