5f1432ae68f3f61c2c40d39dd6176e15494b1509
jackevans47 <jack_evans47@outlook.com>
2024-06-24 17:49:18 +0100

    Add CSS Styling

--- library/settings.py
+++ library/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
 # Default primary key field type
@@ -132,3 +133,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
+
+
+STATICFILES_DIRS = [BASE_DIR / "static"]
